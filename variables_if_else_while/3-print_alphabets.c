938978f8323b0129e4df5a9fc14188d3d6d2d94b
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-14 16:58:05 -0500

    c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ *main - alphabet
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+char al;
+char alm;
+
+for (al = 'a' ; al <=  'z' ; al++)
+{
+	putchar(al);
+}
+for (alm = 'A' ; alm <= 'Z' ; alm++)
+{
+	putchar(alm);
+}
+putchar('\n');
+return (0);
