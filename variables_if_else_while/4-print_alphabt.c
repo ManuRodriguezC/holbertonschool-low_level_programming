49ea7263625f847959cb75152779a920f438618e
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-14 18:56:34 -0500

    shell

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - q and e
+ *
+ *Return:0
+ */
+
+int main(void)
+{
+char al;
+
+for (al = 'a'; al <= 'z'; al != 'q', 'e'; al++)
+{
+	putchar(al)
+}
+putchar('\n')
+return (0)
+}
