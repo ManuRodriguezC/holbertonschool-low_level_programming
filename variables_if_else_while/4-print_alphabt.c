9a69ee9d007fe3838aad192e888e7c0bf8dfa710
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-14 21:41:41 -0500

    c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-char al;
+	char al;
 
-for (al = 'a' ; al <= 'z' ; al++)
-{
-if (al != 'e' || al != 'q')
-putchar(al);
-}
-putchar('\n');
-return (0);
+	for (al = 'a' ; al <= 'z' ; al++)
+	{	
+		if (al != 'e' || al != 'q')
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
 }
