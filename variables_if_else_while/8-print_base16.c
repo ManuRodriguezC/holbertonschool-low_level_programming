b1659250707886d5b2e6b9893a8bdb7c23418ef6
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-14 20:02:41 -0500

    c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - hexadecimal
+ *
+ *Return: 0
+ */
+int main(void)
+{
+char n;
+char al;
+
+for (n = '0' ; n <= '9' ; n++)
+{
+putchar(n);
+}
+for (al = 'a' ; al <= 'f' ; al++)
+{
+putchar(al);
+}
+putchar('\n');
+return (0);
+}
