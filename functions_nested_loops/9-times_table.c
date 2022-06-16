68b6454bbdd2b4b043f505086db0920cbdfbe48c
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-16 00:36:22 -0500

    c[B

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *times_table - mult
+ *
+ *
+ */
+void times_table(void)
+{
+int n, t, m;
+for (n = 0 ; n <= 9 ; n++)
+	for (t = 0 ; t <= 9 ; t++)
+	{
+		m = (n * t);
+		_putchar('m');
+		_putchar(',');
+		_putchar(' ');
+	}
+}
