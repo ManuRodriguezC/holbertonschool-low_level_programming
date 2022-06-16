ddac4901442112e11d9dfaa54ce3324444f5eacc
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-15 23:38:43 -0500

    c[B

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ for (h = 0 ; h <= 23 ; h++)
 	for (n = 0 ; n <= 59 ; n++)
 	{
 		_putchar('h' % 100);
-		_putchar('h' % 10);
+		_putchar(h % 10);
 		_putchar(':');
-		_putchar('n' % 100);
-		_putchar('n' % 10);
+		_putchar(n % 100);
+		_putchar(n % 10);
 		_putchar('\n');
 	}
 }
