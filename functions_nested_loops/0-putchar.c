871dc435e83b9efed924d7107badb7e74d07efb3
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-15 21:04:13 -0500

    c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,12 +6,9 @@
  *Return: 0
  */
 int main(void)
-n = _putchar
 {
-	for (n = _ ; n <= ; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
-	return (0);
+	putchar(putchar);
+}
+putchar('\n');
+return (0);
 }
