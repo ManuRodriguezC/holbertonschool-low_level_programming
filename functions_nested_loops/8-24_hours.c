df0e2438a72044e5468ea3f85b50dae26fdf6d69
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-16 00:02:11 -0500

    c[B

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,14 +7,14 @@
  */
 void jack_bauer(void)
 {
-int n, h;
+int n, h , a, b, c, d;
 for (h = 0 ; h <= 23 ; h++)
 	for (n = 0 ; n <= 59 ; n++)
 	{
-		a = h % 100
-		b = h % 10
-		c = n % 100
-		d = n % 10
+		a = (h % 100);
+		b = (h % 10);
+		c = (n % 100);
+		d = (n % 10);
 		_putchar(a);
 		_putchar(b);
 		_putchar(':');
