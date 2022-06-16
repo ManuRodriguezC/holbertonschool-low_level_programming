c6efb771979846ad8f1b43765456f987c6b27b06
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-15 19:52:35 -0500

    c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-putchar(%c95);
-putchar(p);
-putchar(u);
-putchar(t);
-putchar(c);
-putchar(h);
-putchar(a);
-putchar(r);
+putchar();
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
 
 putchar('\n');
 return (0);
