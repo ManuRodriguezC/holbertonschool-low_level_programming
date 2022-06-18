2e58f2c18e7d21b38a9eafad8a18473c402f666b
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-17 22:24:30 -0500

    c

--- debugging/3-print_remaining_days.c
+++ debugging/3-print_remaining_days.c
@@ -16,9 +16,11 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day <= 60)
 		{
+			day++;
+		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
-		}
+	}
 	else
 	{
 		if (month == 2 && day == 60)
@@ -31,5 +33,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-}
 }
