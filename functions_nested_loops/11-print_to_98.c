ad623f642fc56c0a10fb49595bc92b0bba0b23a4
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-17 21:55:41 -0500

    c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,20 +8,26 @@
 void print_to_98(int n)
 {
 while (n != 98)
-
 	if (n < 98)
 	{
+		print("%i", n);
 		n++;
+		if (n != 98)
+		{
+			printf(",");
+			printf(" ");
+		}
 	}
 	else if (n > 98)
 	{
-		n--;
+		printf("%i", n);
+		if (n != 98)
+		{
+			n--;
+			printf(",");
+			printf(" ");
+		}
 	}
-	else if (n == 98)
-
 printf("%i", n);
-if (n != 98)
-	printf(",");
-	printf(" ");
 return (0);
 }
