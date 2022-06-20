51c89189b9cc3c9f90fcecba506eedc76e4468a1
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-20 10:38:19 -0500

    loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isupper - Diferents in uppercase and others
+ *c@: Lether
+ *Return: 0 if c is upper case, 0 othercase
+ */
+int _isupper(int c)
+{
+if (c <= 'Z')
+	return (1);
+
+else if (c <= 'z')
+	return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+int _isupper(int c);
+int _isdigit(int c);
+
+#endif
