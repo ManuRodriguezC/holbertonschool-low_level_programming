a4ab441036ed8a7f79f542f23f303ef7ba3db0e6
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-20 13:59:47 -0500

    loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers - number string printing
+ *
+ *
+ */
+void print_numbers(void)
+
+for (number = 0 ; number <= 9; number++)
+{
+_putchar(number);
+_putchar('\n');
+}
