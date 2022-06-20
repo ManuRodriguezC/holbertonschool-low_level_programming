0e84620d4ad80cbb81e4c8b16582ec9fb51852d9
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-20 14:25:03 -0500

    loops

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - everythings less 2 and 4
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+int number;
+for (number = '0' ; number <= '9' ; number++)
+	if (number != '2' && number != '9')
+		_putchar(number);
+_putchar('\n');
+}
