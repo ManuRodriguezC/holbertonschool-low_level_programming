7cede2d7ac6c59529ac1fe140678b24074d35341
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-20 18:00:34 -0500

    loops

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *more_numbers - chain of numbers
+ *
+ *
+ */
+void more_numbers(void)
+{
+int list;
+int numbers;
+for (list = 0 ; list <= 10 ; list++)
+	_putchar('\n');
+	for (numbers = 0 ; numbers <= 14 ; numbers++)
+		_putchar(numbers);
+_putchar('\n');
+}
