39b3396a7cfdff5b11196636ab18671ddaf7c455
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-20 19:51:24 -0500

    loops

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,9 @@ int numbers;
 
 for (list = '0' ; list <= '9'  ; list++)
 {
-	for (numbers = '0' ; numbers <= '9' ; numbers++)
+	for (numbers = '0' ; numbers <= '14' ; numbers++)
+		_putchar(numbers);
 
-	if (number > 9)
-		_putchar((i / '10') + '0');
-		_putchar((i % 10) + '0');
-
-
-_putchar(numbers);
+_putchar('\n');
 }
 }
