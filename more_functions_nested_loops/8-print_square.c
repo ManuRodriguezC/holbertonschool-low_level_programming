9cac59fffa5e026e69d24889f731640986712033
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-21 15:18:50 -0500

    loops

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,14 +11,12 @@ int l, w;
 if (size <= 0)
 	_putchar('\n');
 else
-	while (l < size)
+	for (l = 0 ; l < size; l++)
 	{
-		while (w < size)
+		for (w = 0 ; w < size; w++)
 		{
 			_putchar('#');
-			w++;
 		}
 		_putchar('\n');
-		l++;
 	}
 }
