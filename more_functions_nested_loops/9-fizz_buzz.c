83aa7d7b8f5d7f3189eac730420c71ca965942af
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-21 13:08:55 -0500

    loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,12 +3,33 @@
 /**
  *main - conteo 1 al 100
  *
- *
+ *Return: 0
  */
-void main(void)
+int main(void)
 {
 int number;
 for (number = 1; number <= 100 ; number++)
+	if (number % 3 == 0)
+	{
+		printf("Fizz");
+		printf(" ");
+	}
+	else
+		if (number % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+	else
+		if (number % 15 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+	else
+	{
 	printf(number);
 	printf(" ");
+	}
+return (0);
 }
