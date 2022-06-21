3213d44269085dd1c593fcf0a2363038cf8182ed
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-21 18:08:25 -0500

    pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int - two numbers diferents
+ *@*a: one number
+ *@*b: two number
+ *@a: swap number a
+ *@b: swap number b
+ *
+ */
+void swap_int(int *a, int *b)
+{
+_putchar(*a);
+_putchar(*b);
+}
