982df6fd720bbdf9fff98c3574b13b88dae716e9
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-20 20:16:57 -0500

    loops

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_line - line line line
+ *@n: number is times is repeted
+ *
+ */
+void print_line(int n)
+{
+	{
+	if (n != 0)
+		{
+		_putchar('_');
+		n++
+		}
+	}
+_putchar('\n');
+}
