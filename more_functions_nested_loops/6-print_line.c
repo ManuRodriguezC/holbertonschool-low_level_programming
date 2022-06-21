015f6d53d7b56728abbb7229fd1ab8204c7859b5
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-20 22:15:45 -0500

    loops

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,10 @@ void print_line(int n)
 while (n != 0)
 	{
 	if (n > 0)
-		{
+	{
 		_putchar('_');
 		n--;
-		}
+	}
 	}
 _putchar('\n');
 }
---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_diagonal - linea diagonal
+ *@n: number print
+ *
+ */
+void print_diagonal(int n)
+{
+while (n != 0)
+	if (n < 0)
+	{
+		_putchar(\);
+		n--;
+	}
+_putchar('\n')
+}
