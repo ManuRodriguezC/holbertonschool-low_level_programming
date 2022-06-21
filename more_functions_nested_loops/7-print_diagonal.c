417700757ca6fc61fec214d39e75cebb736c6e42
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-21 10:45:20 -0500

    loops

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,7 +11,7 @@ void print_diagonal(int n)
 	{
 		_putchar(' ');
 		n--;
-		_putchar(92);
+		_putchar(92;
 		_putchar('\n');
 	}
 _putchar('\n');
---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_square - 10x10
+ *@size: numbers of times
+ *
+ */
+void print_square(int size)
+{
+while (size > 0)
+{
+	while (size > 0)
+	{
+		_putchar(35);
+		size--;
+	}
+	_putchar('\n');
+	size--;
+
+}
+putchar('\n');
+}
