67eaba3e0bf767012406c3c5d7fb826fc8d2e7cd
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-21 10:02:09 -0500

    loops

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,15 @@
  */
 void print_line(int n)
 {
-while (n != 0)
-	{
-	if (n > 0)
-	{
-		_putchar('_');
-		n--;
-	}
-	}
-if (n <= 0)
+{
+if (n > 0)
+{
+	_putchar('_');
+	n--;
+}
+
+else
+	if (n <= 0)
+}
 _putchar('\n');
 }
