6a844615e0dec50ed2e411f3ee08e961197d4f33
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-21 14:39:28 -0500

    loops

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,9 +7,15 @@
  */
 void print_square(int size)
 {
-for (; size )
-{
-	for ()
-}
-_putchar('\n');
+int l, w;
+if (n <= 0)
+	_putchar('\n');
+
+for (l = 0 ; l < size ; l++)
+	for (w = 0 ; w < l ; w++)
+		if (l == w)
+			_putchar('#');
+			_putchar('\n');
+		else
+			_putchar('#');
 }
