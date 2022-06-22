57ccd71766a3a0bd1450d6a265ebeecab5834e91
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-22 16:19:57 -0500

    print string in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,12 @@
 void print_rev(char *s)
 {
 int tam, w;
+char invs;
 
 strcpy (invs, s);
 
 tam = strlen(s);
-for (w = 0; w < rev ; w++)
+for (w = 0; w < tam ; w++)
 {
 	invs[w] = s[tam - 1 - w];
 }
