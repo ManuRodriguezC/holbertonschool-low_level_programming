aa763290594a29ab860ca195a06a6e7e72194945
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-22 20:41:55 -0500

    rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *print_rev - revers of string
+ *@*s: pointer s
+ *@s: variable
+ *
+ */
+void print_rev(char *s)
+{
+int x, i;
+int t = 0;
+_putchar(s);
+
+for (i = 0 ; s[i] != '\0' ; i++)
+t++;
+for (x = (t - 1) ; x >= 0 ; x--)
+_putchar(s[x]);
+}
