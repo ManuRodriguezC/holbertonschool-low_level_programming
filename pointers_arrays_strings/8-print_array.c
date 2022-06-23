c16088a05a3990dfb75e988277f5162fcddd1279
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-23 10:11:31 -0500

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *print_array - vector int
+ *@a: a
+ *@n: n
+ *
+ */
+void print_array(int *a, int *n)
+{
+int leni, i;
+
+len = strlen(array)
+
+for (i = 0 ; i <= len ; i++)
+	printf(array[i]);
+		if (i != len)
+			printf(',');
+			printf(' ');
+}
