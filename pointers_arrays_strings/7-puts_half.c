1ed39330f0cccc75c6eb561e124b83f84ec11970
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-22 23:09:44 -0500

    any numbers

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,9 @@ void puts_half(char *str)
 {
 int len, i, j;
 len = (strlen(str) / 2);
-if (len % 2 == 0)
-	for (i = len ; str[i] != '\0' ; i++)
+for (i = len ; str[i] != '\0' ; i++)
 		_putchar(str[i]);
-else 
-	for (j = (len - 1) ; str[j] != '\0' ; j++)
-		_putchar(str[j]);
+
 _putchar('\n');
 
 }
