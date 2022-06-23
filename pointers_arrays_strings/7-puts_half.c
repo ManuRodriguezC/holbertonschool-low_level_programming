bd64371193257f9342b64d00089f05ced8dbfd55
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-23 10:25:46 -0500

    print string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,10 @@ void puts_half(char *str)
 int len, i;
 len = (strlen(str) / 2);
 if (len % 2 == 0)
-	for (i = len ; str[i] != '\0' ; i++)
+	for (i = (len - 1) ; str[i] != '\0' ; i++)
 		_putchar(str[i]);
 else
-	for (i = (len - 1) ; str[i] != '\0' ; i++)
+	for (i = len ; str[i] != '\0' ; i++)
 		_putchar(str[i]);
 _putchar('\n');
 
