1b1e7134565c14cffcf27a7aadcfc1474e6cc324
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-23 12:49:28 -0500

    cpy string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,18 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 /**
- *
- *
- *
+ **_strcpy - copy string
+ *@dest: new string
+ *@str: orogin string
  */
+char *_strcpy(char *dest, char *src)
+{
+puts(src);
+
+dest = srtcpy(src);
+
+puts(dest);
+
+}
