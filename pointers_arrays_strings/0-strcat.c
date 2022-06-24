4dd214784ff75f4e2a9c0bdc4e36322ccfd37433
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-23 23:10:39 -0500

    cat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,10 @@
 char *_strcat(char *dest, char *src)
 {
 int i, j;
-int t = 0;
 
-for (i = 0 ; dest[i] != '\0'; i++)
-t++;
+for (i = 0 ; dest[i] != '\0' ; i++)
+for (j = 0 ; dest[i + j] = *src++ ; j++)
 
-for (j = 0 ; src[j] == '\0' ; j++)
-dest[t + j] = src[j];
 
 return (dest);
 }
