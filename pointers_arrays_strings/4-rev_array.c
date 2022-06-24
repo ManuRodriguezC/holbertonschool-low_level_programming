00eba7eb9bb62069f2669f03f566d40643150ef8
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-24 15:57:27 -0500

    rev array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *reverse_array - yarra
+ *@n: numbers
+ *@a: array
+ *Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+int i, x;
+
+for (i = 0 ; i < n ; i++)
+
+for (x = 0 ; x < i ; x++)
+	{
+	i--;
+	printf(a[x]);
+	}
+return (0);
+}
