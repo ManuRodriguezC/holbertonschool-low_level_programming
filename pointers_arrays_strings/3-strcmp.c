abe4518aa81f18c902372d5e8b43b441997113f4
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-24 11:15:47 -0500

    copy byts

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strcmp - Comparing two strings
+ *@s1: string one
+ *@s2: string two
+ *Return: -1, 0, 1|
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i, j, t1, t2;
+
+for (i = 0 ; s1[i] != '\0' ; i++)
+t1++;
+for (j = 0 ; s2[j] != '\0' ; j++)
+t2++;
+
+if (t1 > t2)
+	return (-1);
+else if (t2 > t1)
+	return (1);
+else if (t1 == t2)
+	return (0);
+}
