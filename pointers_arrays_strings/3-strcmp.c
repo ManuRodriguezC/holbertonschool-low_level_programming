7fe8357e82fcc5a7c9f494e6277ab5397b88812e
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-25 17:54:30 -0500

    compared

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i, j;
+int i, j, r;
 int t1 = 0;
 int t3 = 0;
 int t2 = 0;
@@ -21,9 +21,9 @@ for (i = 0 ; s1[i] != '\0' ; i++)
 for (j = 0 ; s2[j] != '\0' ; j++)
 	t2++;
 if (t1 < t2)
-	return (-13);
+	r = (13);
 else if (t2 < t1)
-	return (13);
+	r = (-13);
 else if (t1 == t3)
-	return (0);
+	r = (0);
 }
