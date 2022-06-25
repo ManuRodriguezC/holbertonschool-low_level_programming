7c64f3b8673d99877b3bfee78d3dbd238300e2cd
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-25 18:53:48 -0500

    cap

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - string changes
+ *@a
+ *
+ */
+char *cap_string(char *a)
+{
+int i;
+
+for (i = 0 ; a[i] < '\0' ; i++)
+	if (a[i] == " ")
+		if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+			a[i] -= 'a' - 'A';
+return (a);
+}
