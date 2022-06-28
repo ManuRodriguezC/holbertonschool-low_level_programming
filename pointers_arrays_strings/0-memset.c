c6b0dfc83b7c0e23cc5eafd3b8ccbbfdf5a3b5ec
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-28 11:53:55 -0500

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * *_memset - n changes
+ *@s: string
+ *@b: constan
+ *@n: number changes
+ *Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 int i, j;
 
 for (i = 0 ; s[i] != '\0' ; i++)
-	for (j = 0 ; j < n ; j++)
-		s[j] -= b;
-	i++;
+
 return (s);
 }
