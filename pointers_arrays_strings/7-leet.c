b28a70d64eae7506e965be700722e881d65739e6
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-28 11:05:59 -0500

    changes strings

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,18 +2,23 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * *leet - changes some caracters
+ *@s: string
+ *Return: a
  */
 char *leet(char *a)
 {
 int i;
-
-for (i = 0 ; a[i] < '\0' ; i++)
-
-
-
-
+for (i = 0 ; s[i] != '\0' ; i++)
+	if (s[i] = 'a' && s[i] = 'A')
+		s[i] = 4;
+	else if (s[i] = 'e' && s[i] = 'E')
+		s[i] = 3;
+	else if (s[i] = 'o' && s[i] = 'O')
+		s[i] = 0;
+	else if (s[i] = 't' && s[i] = 'T')
+		s[i] = 7;
+	else if (s[i] = 'l' && s[i] = 'L')
+		s[i] = 1;
 return (a);
 }
