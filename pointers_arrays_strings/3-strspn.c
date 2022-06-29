37e6a08aa7032a618b9561611cef3daf60902f29
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-29 15:28:25 -0500

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,3 +7,13 @@
  *
  */
 unsigned int _strspn(char *s, char *accept)
+{
+int i, j, cont;
+while (s[i] == accept[j])
+{
+	for (i = 0 ; s[i] != '\0' ; i++)
+	for (j = 0 ; accept[j] != '\0' ; j++)
+cont++;
+}
+return (cont);
+}
