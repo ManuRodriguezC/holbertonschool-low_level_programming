bc1ba76de1fa25bd2fbefe801376f4683fa8e9e4
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-29 11:41:52 -0500

    memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memcpy - copy n bytes
+ *@des: destin
+ *@src: orogin
+ *@n: bytes
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i;
+
+while (s[i] < n && s[i] != '\0')
+{
+	dest = s[i];
+	i++;
+}
+return (dest);
+}
