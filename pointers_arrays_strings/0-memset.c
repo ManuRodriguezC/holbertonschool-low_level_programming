9a736aa71080cba200c91880746fc2ef58760a87
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-29 13:03:16 -0500

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -12,11 +12,13 @@ char *_memset(char *s, char b, unsigned int n)
 {
 int i = 0;
 int j;
-for (j = 0 ; j < n ; j++)
+for (j = 0 ; s[j] < n ; j++)
 
 while (i < j && s[i] != '\0')
-	if (s[i] < n)
-		s[i] = b;
+{
+if (s[i] == s[j])
+	s[i] = b;
 i++;
+}
 return (s);
 }
---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
