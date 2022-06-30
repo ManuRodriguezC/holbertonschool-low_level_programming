a4569db103633a2a460c160835cb7653879e8050
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-30 17:19:45 -0500

    strlen

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen_recursion - count
+ *@s: string
+ *Return: i
+ */
+int _strlen_recursion(char *s)
+{
+int i = 0;
+if (s[0] != '\0')
+	i++;
+	_strlen_recursion(s + 1);
+return (i);
+}
