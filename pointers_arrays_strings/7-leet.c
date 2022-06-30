9a36b5aae36ef4a9ce94b35676bb94d87539eb2e
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-30 01:15:15 -0500

    print

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,26 +8,16 @@
  */
 char *leet(char *s)
 {
-int i;
-for (i = 0 ; s[i] != '\0' ; i++)
+int i, j;
+char letter[] = "aAeEoOtTlL"
+char numbers[] = "4433007711"
+for (i = 0 ; *(s + i) ; i++)
 {
-switch (s[0])
+for (j = 0 ; j < 10 ; j++)
 {
-	case s[0] == 'a' && s[0] == 'A':
-		s[0] -= 4;
-		break;
-	case s[0] == 'e' && s[0] == 'E':
-		s[0] -= 3;
-		break;
-	case s[0] == 'o' && s[0] == 'O':
-		s[0] -= 0;
-		break;
-	case s[0] == 't' && s[0] == 'T':
-		s[0] -= 7;
-		break;
-	case s[0] == 'l' && s[0] == 'L':
-		s[0] -= 1;
-		break;
+	if (letter[j] == *(s + i))
+		*(s + i) = numbers[j];
+}
 }
 return (s);
 }
