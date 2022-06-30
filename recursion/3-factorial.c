1a0f5533aa8d0b612f709f7d07230a625ed2bfae
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-30 17:29:19 -0500

    factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *factorial - factorial of a number
+ *@n: number
+ *Return: -1, n
+ */
+int factorial(int n)
+{
+if (n < 0)
+	return (-1);
+else
+	return(n * factorial(n - 1))
+}
