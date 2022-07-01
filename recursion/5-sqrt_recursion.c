471bfd8f1d5e8a03aa2d32fe21e81971bab9cc7b
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-01 17:28:04 -0500

    square

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *_sqrt_recursion - square root
+ *@n: number
+ *Return: -1, n
  */
 int _sqrt_recursion(int n)
 {
+int i = n;
 if (n < 0)
 	return (-1);
-
-
+if ((n * n) != i)
+	_sqrt_recursion(n - 1);
+if (n == i)
+	return (n);
 }
