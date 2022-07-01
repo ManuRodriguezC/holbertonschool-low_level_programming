d09e2efe0070fe4c7a9e067e1af4fecb61d163e2
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-01 12:35:04 -0500

    string

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,13 +7,10 @@
  */
 void _puts_recursion(char *s)
 {
-	{
-	if (*s == '\0')
-		return;
+if (*s == '\0')
+	return;
 
-	if (*s != '\0')
-		_putchar(*s);
-	_puts_recursion(s + 1);
-	}
-_putchar('\n');
+if (*s != '\0')
+	_putchar(*s);
+_puts_recursion(s + 1);
 }
