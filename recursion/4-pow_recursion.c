b1531e618f2774462197bda161cbb74db58fbb02
ManuRodriguezC <estebanclimb@gmail.com>
2022-06-30 21:50:41 -0500

    raised

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_pow_recursion - raised to the power
+ *@x: number
+ *@y: raised
+ *Return: -1, 1
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+	return (-1);
+}
+else if (y <= 1)
+{
+	return (1);
+}
+else
+{
+	return (x * _pow_recusion(y - 1));
+}
+}
