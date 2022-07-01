3b1844050fd4a9d3be03c50ae79f67d10eaffba1
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-01 12:53:12 -0500

    len

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,12 +8,9 @@
  */
 int _strlen_recursion(char *s)
 {
-int i = 0;
-if (s[0] == '\0')
-		return;
-if (s[0] != '\0')
-_strlen_recursion(s + 1);
-	(s[0] != '\0')
-	i++;
-return (i);
+
+if (*s == '\0')
+	return (0);
+
+return (1 + _strlen_recursion(s + 1));
 }
