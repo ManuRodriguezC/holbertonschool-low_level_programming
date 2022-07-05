6c56381a084e3961f27be3ef28c9e0f1cce0a32f
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-05 15:53:06 -0500

    arguments

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - print name
+ *@argc: number caracters
+ *@argv: caracter
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+	_putchar(argv[i]);
+}
+return (0);
+}
