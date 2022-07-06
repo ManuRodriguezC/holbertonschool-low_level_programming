37a9d578c76cb8ac8177cb97592f16033c43e91b
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-05 19:31:51 -0500

    mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - print name
+ *@argc: number caracters
+ *@argv: caracter
+ *Return: 1
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int mul = 0;
+
+if (argc > 0)
+{
+	for (i = 0 ; i < argc ; i++)
+	{
+	mul = argc[i] * argv[i + 1]
+	printf("%d\n", mul);
+	}
+}
+printf("Erro");
+return (1);
+}
