e49a135184e218788c23c5e69ccb372996e48438
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-05 19:02:55 -0500

    argc

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,14 +10,13 @@
  */
 int main(int argc, char *argv[])
 {
-	{
-	int i, j;
-		{
-		for (i = 0 ; i < argc ; i++)
+int i, j;
+{
+	for (i = 0 ; i < argc ; i++)
 
-		for (j = 0 ; j < *argv[i] ; j++)
-		}
-	printf("%d", argc);
-	}
+	for (j = 0 ; j < *argv[i] ; j++)
+}
+printf("%d", argc);
+
 return (0);
 }
