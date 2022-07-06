e4c3f1408146ec42bfe4afc679c3e6428c630eeb
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-06 17:28:41 -0500

    sum

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,24 +10,25 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-
+int i, j;
+{
+for (j = 0 ; j <= 'a' && j >= 'z' ; j++)
+}
 for (i = 0; i < argc ; i++)
 {
 	if (argv[i] == 0)
 	{
-		printf("0\n");
+		printf("'0'\n");
 	}
-	if (argv[i] == (argv[i] >'a' && argv[i] < 'z'))
+	if (argv[i] == j)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	if (argv[i] > 0)
 	{
-		sum += argv[i];
+		sum += strtol(argv[i]);
 	}
 	printf("%d\n", sum);
 }
 return (0);
-}
