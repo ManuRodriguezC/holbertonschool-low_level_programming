3708bf40fbe6746bcfd66603f5ab9020622a46ce
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-05 19:13:32 -0500

    args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print name
+ *@argc: number caracters
+ *@argv: caracter
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc ; i++)
+{
+	printf("%s\n", argv[i]);
+}
+return (0);
+}
