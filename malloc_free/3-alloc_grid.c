66787a453e4c49713fa8e1b7db2761a2f9141ab6
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-07 08:30:36 -0500

    dimensional array

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * **allac_agrid - dimensional array
+ *@width: an
+ *@height: al
+ *Return: mat
+ */
+int **alloc_grid(int width, int height)
+{
+char *mat = NULL;
+unsigned int w, h;
+
+if (width <= 0 || height <= 0)
+	return (NULL);
+
+lenw = strlen(width);
+lenh = strlen(height);
+
+mat = malloc(sizeof(*mat) * lenh * lenw)
+
+if (!mat)
+	return (NULL);
+
+for (w = 0 ; width[w] < width ;)
+	for(h = 0 ; height[h] < height ; j++)
+		mat[w] = width[w];
+mat[w] = height[w][h];
+w++;
+
+return (mat);
+free(mat);
+}
