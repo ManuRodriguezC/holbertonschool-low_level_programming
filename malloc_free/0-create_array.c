cea6de4620101ae9dac0d78d95c717a3458e070a
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-06 19:18:24 -0500

    array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+new = malloc(sizeof(char) * size)
+
+if (!new)
+	return (NULL);
+
+
+}
