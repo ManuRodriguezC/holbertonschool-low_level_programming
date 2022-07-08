dcde32816152f001c66cef82cea0593f8d4b14e0
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-07 21:24:28 -0500

    alloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int i, j;
 if (width <= 0 || height <= 0)
 	return (NULL);
 
-mat = malloc(sizeof(int *) * (height * height));
+mat = malloc(sizeof(int *) * (width * width));
 if (!mat)
 	return (NULL);
 
