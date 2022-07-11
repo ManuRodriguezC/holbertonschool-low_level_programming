016b15c28c8894d9132603cf09a5e8c7d38fccb2
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-10 20:02:43 -0500

    min-max

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+int *array_range(int min, int max)
+{
+int *list, i, len;
+
+if (min > max)
+	return (NULL);
+
+len = (max - min);
+
+list = malloc(sizeof(int) * (len + 1));
+
+if (list == NULL)
+	return (NULL);
+
+for (i = 0 ; i <= len ; i++)
+	list[i] = min++
+
+return (list);
+}
