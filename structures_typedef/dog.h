356aca0f441b1358103ee80f38000c543b992f90
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-11 17:51:22 -0500

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,5 @@
-typedef struct dog
-{
-char * name;
-char * owner;
-float age;
-} dog;
-
-int main(void)
-{
-struct Dog dog;
-dog dog2;
-
-return (0);
-}
+struct dog {
+	char * name;
+	char * owner;
+	float age;
+};
