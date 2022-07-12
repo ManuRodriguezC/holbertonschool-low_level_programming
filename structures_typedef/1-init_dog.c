9cf9768256d580f62e3aa89280a11fe9bf27f1ee
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-12 11:52:02 -0500

    init

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,8 +10,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-{d->name = name, d->age = age, d->owner = owner; }
-
 if (d == NULL)
-	return (0);
+	{d->name = name, d->age = age, d->owner = owner; }
 }
