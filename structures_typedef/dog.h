d90d60d995a4143111a355b18b0834932baa9227
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-11 21:12:50 -0500

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 struct dog
 {
 	char *name;
@@ -8,4 +9,4 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#endif /*struct dog*/
+#endif
