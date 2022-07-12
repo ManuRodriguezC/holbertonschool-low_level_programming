d098a3fbb3c35c1235a7c72795abc2b69a95dad4
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-11 21:19:17 -0500

    init

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *init_dog - funcion
+ *@*d: dog
+ *@*name: name
+ *@age: age
+ *@owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+
+}
