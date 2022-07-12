da7fc7dce0807227b0e88a6abdf3aae3b1f3c5aa
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-12 09:00:33 -0500

    init

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,12 +3,12 @@
 
 /**
  *init_dog - funcion
- *@*d: dog
- *@*name: name
+ *@d: dog
+ *@name: name
  *@age: age
  *@owner: owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog = {"Milo", 3.5, "Bob"};
+struct dog = { "abc", 3.50, "abz"};
 }
