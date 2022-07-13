2f142d9a57f8688f6d6c182a79e60fc998572a33
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-12 22:46:58 -0500

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,11 +4,12 @@
 #include <stdlib.h>
 
 /**
- *dog_t - new dog
+ *dog_t *new_dog - new dog
  * *new_dog: new
  *@name: dog name
  *@age: dog age
  *@owner: owner
+ *Return: d
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
