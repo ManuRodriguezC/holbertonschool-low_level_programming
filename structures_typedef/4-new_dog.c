c9b99d9cc4bca3fb0f479dec0caebdaaba271d9c
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-12 22:40:55 -0500

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,32 +11,32 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *new_name = NULL;
-char *new_owner = NULL;
-struct dog *d = NULL;
+	char *new_name = NULL;
+	char *new_owner = NULL;
+	struct dog *d = NULL;
 
-new_name = malloc(sizeof(*new_name) * strlen(name) + 1);
-new_owner = malloc(sizeof(*new_owner) * strlen(owner) + 1);
+	new_name = malloc(sizeof(*new_name) * strlen(name) + 1);
+	new_owner = malloc(sizeof(*new_owner) * strlen(owner) + 1);
 
-if (new_name || new_owner == NULL)
-	return (NULL);
+	if (new_name || new_owner == NULL)
+		return (NULL);
 
-strcpy(new_name, name);
-strcpy(new_owner, owner);
+	strcpy(new_name, name);
+	strcpy(new_owner, owner);
 
-d = malloc(sizeof(struct dog));
+	d = malloc(sizeof(struct dog));
 
-if (d == NULL)
-{
-	free(d);
-	free(new_name);
-	free(new_owner);
-	return (NULL);
-}
+	if (d == NULL)
+	{
+		free(d);
+		free(new_name);
+		free(new_owner);
+		return (NULL);
+	}
 
-d->name = new_name;
-d->age = age;
-d->owner = new_owner;
+	d->name = new_name;
+	d->age = age;
+	d->owner = new_owner;
 
-return ();
+	return (d);
 }
