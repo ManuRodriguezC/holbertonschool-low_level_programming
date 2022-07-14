86b5ca146098bbf9cb4ca7ef9b0b82bcfc6b8a61
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-14 10:56:58 -0500

    get

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "3-calc.h"
+#include <strig.h>
 
 /**
  *(*get_op_func - call fucntions)
@@ -8,7 +9,20 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-char funciones = "+-/*%";
-if (s == funciones)
-	return (s);
+op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+    };
+    int i = 0;
+
+    while (i < 5)
+    {
+    if (ops[i].op == s)
+	    return (ops[i].f);
+    i++;
+    }
 }
