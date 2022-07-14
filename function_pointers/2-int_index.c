bd2bbc1150e509505e96b3cdc092df4546b831c4
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-13 20:24:31 -0500

    int

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,8 +2,26 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
+ *int_index - array
+ *@array: array
+ *@size: size
+ *@cmp: funtion
+ *Return:-1, 0, 1
  */
 int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (!cmp)
+	return (0);
+
+for (i = 0; i < size; i++)
+
+if (size <= 0)
+	return (-1);
+
+int (*fp)(int);
+fp = cmp;
+(*fp)(array[i])
+
+
+}
