9b876490fd8d5ea8408277d23560277791c0a5d0
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-13 20:38:53 -0500

    int

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,9 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i;
 if (!cmp && !array)
 	return (0);
-
+{
 int (*fp)(int);
 fp = cmp;
 for (i = 0; i < size; i++)
 (*fp)(array[i]);
+}
 }
