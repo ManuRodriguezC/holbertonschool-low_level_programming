35ffa5e13fb136f689635c6270b462d57f5f1795
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-15 16:56:35 -0500

    numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <variadic_functions.h>
+#include <stdarg.h>
+
+/**
+ *print_numbers - numbers
+ *@separator: separators
+ *@n: number of times
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list ap;
+unsigned int i;
+int j;
+
+if (separator == '\0')
+	return (0);
+
+va_start(ap, n);
+
+for (i = 1; i <= n; i++)
+{
+	printf("%d", va_arg(ap, int));
+	for (j = 0; separator[j] != '\0'; j++)
+	{
+	if (i < n)
+		printf("%c", separator[j]);
+	}
+}
+va_end(ap);
+printf("\n");
+}
