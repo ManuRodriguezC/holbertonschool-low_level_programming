830c42fcb354a62c432603b7f63077305a337334
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-14 21:15:10 -0500

    calc

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -7,23 +7,38 @@
  *@s: character
  *Return:s
  */
+int includes(char *string, char *substring)
+{
+	if (string == NULL || substring == NULL)
+		return (0);
+	
+	if (*string == '\0')
+		return (1);
+	
+	if (*string == *substring)
+		return (includes(string + 1, substring +1));
+
+	return (0);
+
+}
+
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
+	int i = 0;
+	op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}
 	};
-int i = 0;
 
-while (i < 5)
-{
-	if (strcmp(ops[i].op, s) == 0)
-		return (ops[i].f);
-	i++;
+	while (ops[i].op != NULL)
+	{
+		if (includes(ops[i].op, s))
+			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
