1ba4fb504cdad59c8a68d84d137c297a0b8fc6d0
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-15 11:30:49 -0500

    variable

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *sum_them_all - sum everyting
+ *@n: const
+ *Return: sum, 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list ap;
+const unsigned int i, sum = 0;
+
+if (n == 0)
+	return (0);
+
+va_start (ap, n);
+
+for (i = 0; i < n; i++)
+	sum += va_arg (ap, const unsigned int);
+
+va_end(ap);
+return (sum);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifdef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNTIONS_H
+
+/* importing */
+#include <stdio.h>
+#include <stdarg.h>
+
+/* prototypes */
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
