40a4756a03c4549a0ff482a370c7661cd1a02520
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-26 17:11:15 -0500

    Print all int elements of the listint

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *print_listint - Print all int elements of the list
+ *@h: Singly linked list
+ *Return: count is the number of elements in the listint
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t count;
+
+while (h != NULL)
+{
+	printf("%d", h->n);
+	h = h->next;
+	count++;
+}
+return (count);
+}
