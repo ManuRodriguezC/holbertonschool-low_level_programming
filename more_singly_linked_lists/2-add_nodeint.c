3f9d7646837d346a4c8a18ea404c0913a5d0c3b5
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-26 17:40:28 -0500

    Adds of differents elements in the list

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *add_nodeint - Add a new node at the begin od the list
+ *@head: head of the list
+ *@n: Value to add to the new node}
+ *Return: Address of the new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new;
+new = *head;
+
+new = malloc(sizeof(listint_t));
+
+if (new == NULL)
+	return (NULL);
+
+new->n = n;
+next->next = *head;
+*head = new;
+return (*head);
+}
