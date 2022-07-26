19f1db4f6d73cc6ab6154d37254a368a5cc819cc
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-26 16:33:18 -0500

    Free memory of the list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_list - Free memory of the linked
+ *head_ The beginning of the list_t
+ */
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while ((temp = head) != NULL)
+{
+head = head->next;
+free(temp->str);
+free(temp);
+}
+}
