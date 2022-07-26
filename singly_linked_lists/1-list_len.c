6bd8cabccafd7b853c74b8245ce058c79f7ccb8e
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-26 13:34:41 -0500

    Count the all elements in the list_t

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+
+/**
+ *list_len - Count the number of elements in the list
+ *@h: Value od the leng str
+ *Return: Count is number of elements
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+	h = h->next;
+	count++;
+}
+return (count);
+}
