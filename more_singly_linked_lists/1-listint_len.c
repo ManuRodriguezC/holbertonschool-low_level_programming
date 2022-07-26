7292531a77c4f078eb5d5ca1a3e6eb770df8239a
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-26 17:32:12 -0500

    Number of elements of the list

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ *listint_len - Number of elements of the list
+ *@h: Singly linked list
+ *Return: count is number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t count = 0;
+
+for (; h; count++)
+
+return (count);
+}
