60a7accca6a3595dc653bb7762e30c702eacb853
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-27 08:25:55 -0500

    Elemente delete in the head

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *pop_listint - Delete head of the node in the linked list
+ *@head: The head of the linked list
+ *Return: Date in the element delete
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *new;
+int n = 0;
+
+if (*head != NULL)
+{
+new = (*head)->n;
+n = (*head)->n;
+free(*head);
+*head = new;
+}
+return (n);
+}
