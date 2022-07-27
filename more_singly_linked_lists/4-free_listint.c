7af775edd6e5ec100b8de9520048a95159dd40d3
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-27 07:56:40 -0500

    Free of the head list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_listint - Free linked list
+ *@head: The head of the linked list
+ *
+ */
+void free_listint(listint_t *head)
+{
+listint_t * temp;
+
+while ((temp = head) != NULL)
+{
+head = head->next;
+free(temp);
+}
+}
