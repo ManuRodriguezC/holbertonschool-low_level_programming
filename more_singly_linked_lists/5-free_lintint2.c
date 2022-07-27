2ff93dda10349b5c20dab95d1bd19a281faa455d
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-27 08:14:54 -0500

    more free

--- more_singly_linked_lists/5-free_lintint2.c
+++ more_singly_linked_lists/5-free_lintint2.c
@@ -1,12 +1,22 @@
 #include "lists.h"
 
 /**
- *
- *
+ *free_listint2 - Free linked list
+ *@head: The head of the linked
  *
  */
 void free_listint2(listint_t **head)
 {
+listint_t *temp;
 
+if (head == NULL)
+	return (NULL);
 
+while (*head)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+*head = NULL;
 }
