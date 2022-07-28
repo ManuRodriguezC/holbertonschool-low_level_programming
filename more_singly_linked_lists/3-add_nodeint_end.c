ee61c147c466278039a7f39e76b5b18b2d8e47ca
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-27 21:49:43 -0500

    add new element to end list

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *new = (listint_t *)malloc(sizeof(listint_t));
+listint_t *new = malloc(sizeof(listint_t));
 listint_t *final = *head;
 
 if (new == NULL)
