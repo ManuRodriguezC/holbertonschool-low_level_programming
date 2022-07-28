1f1ee0e6168f76217980e020f6927e7dfd6d20ee
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-27 22:03:52 -0500

    Get node index

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int count;
 
-for (count = 0; head != NULL; count++; head = head->next)
+for (count = 0; head != NULL; count++, head = head->next)
 if (count == index)
 	return (head);
 return (NULL);
