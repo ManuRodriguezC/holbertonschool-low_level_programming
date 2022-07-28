0ab0444902eb8932762dd1864ad5f70443bca8cd
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-28 09:39:30 -0500

    Sum of numbers elements

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *Write a function that returns the sum of all the data (n) of a list
+ *@haad: The head of the linked list
+ *Return: Sum of the numbers and 0  list is emply
+ */
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+
+if (n == NULL)
+	return (0);
+sum += sum->n;
+n = n->next;
+return (sum);
+
+}
