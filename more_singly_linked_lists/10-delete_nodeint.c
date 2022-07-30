115dc856a475b3dc76332cd5a3a7c3fd8ae2d03c
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-29 21:00:12 -0500

    Delete one node in some position

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,12 +1,39 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *delete_nodeint_at_index - delete one node in somethings position.
+ *@head: linked list
+ *@index: Is a specific position to delete.
+ *Return: 1 if it succeeded, -1 if it failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+listint_t *dele;
+listint_t *next;
+unsigned int position;
 
+dele = *head;
 
+if (index != 0)
+{
+for (pisition = 1; pisition < index && dele != NULL; pisition++)
+{
+dele = dele->next,
+}
+}
+if (dele == NULL || (dele->next == NULL && index != 0))
+	return (-1);
+next = dele->next;
+
+if (index != 0)
+{
+dele->next = next->next;
+free(next);
+}
+else
+{
+free(dele);
+*head = next;
+}
+return (1);
 }
