d0b2776845263b99fef2fde7348d3ebc17930fdc
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-01 18:01:51 -0500

    A binary to decimal

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_binary - Convert decimal to binaty
+ *n: is a decimal number
+ *
+ */
+void print_binary(unsigned long int n)
+{
+char cadena[63];
+int i;
+
+itao(n, cadena, 2);
+
+for (i = 0; cadena[i] != '\0'; i++)
+	_putchar(i);
+}
