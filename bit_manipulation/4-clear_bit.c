7e1df43787cc6bfcd59517abc5cd49b7011abb95
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-02 08:23:40 -0500

    The number give a 0 in index position

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *clear_bit - Write a function that sets the value of a bit to 0 at a given
+ *index.
+ *@n: Is a number to convert.
+ *@index: Is a index
+ *Return: 1 if it worked, or -1 if an error occurred.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (!n)
+		return (-1);
+	*n = 0 << index | *n;
+	return (1);
+}
