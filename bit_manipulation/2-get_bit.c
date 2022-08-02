d47423f87120222db6e8a02d3d95f1d0c561d2fa
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-02 08:00:24 -0500

    Value of a bit at a given index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *get_bit - Write a function that returns the value of a bit at a given index.
+ *@n: Is the number comparete.
+ *@index: Is the index, starting from 0 of the bit you want to get
+ *Return: Value od the bit.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (!n)
+	return (-1);
+return ((n >> index) & 1);
+}
