4a84116deb0e93a553d6a25a9008af76ff9d3306
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-02 08:14:45 -0500

    Number give to index in position

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *set_bit - Write a function that sets the value of a bit to 1 at a given index
+ *.
+ *@n: Is a number to convert.
+ *@index: Index
+ *Return:1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (!n)
+		return (-1);
+	*n = 1 << index | *n;
+	return (1);
+}
