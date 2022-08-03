531798f6945d9b50ad9517f97583411d46c48dee
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-02 21:44:28 -0500

    Append text in file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,6 +18,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
+	
+	if (text_content == NULL)
+		text_content = "";
 
 	for (count = 0; text_content[count] != '\0'; count++)
 	;
