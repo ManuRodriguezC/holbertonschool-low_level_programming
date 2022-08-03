6a86b6785c23fd256091cd3c47be538c05216466
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-02 20:25:00 -0500

    Creat a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *create_file - Create a function that creates a file.
+ *@filename: Name of the file
+ *@text_content: text in the file
+ *Return: write text inside the file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wrfd, count;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0200);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (count = 0; content[count] != '\0'; count++)
+	;
+	wrfd = write(fd, text_content, count);
+
+	if (wrdf == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
