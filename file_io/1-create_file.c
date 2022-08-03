e19d8260d6fff2e0e765ef1f50af14137223239b
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-02 20:28:15 -0500

    Creat a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,9 +17,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
 	for (count = 0; text_content[count] != '\0'; count++)
 	;
 	wrfd = write(fd, text_content, count);
