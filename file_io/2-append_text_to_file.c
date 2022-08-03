1bb2e9ab45733e22cc7ac43bce917af15f66fc9f
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-02 21:43:20 -0500

    Append text in file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *append_text_to_file - Write a function that appends text at
+ *the end of a file.
+ *@filename: Name of the file.
+ *@text_content: Text to appends in file.
+ *Return: 1 if the file exists and -1 if failed.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wrfd, count;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	for (count = 0; text_content[count] != '\0'; count++)
+	;
+
+	wrfd = write(fd, text_content, count);
+	if (wrfd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
