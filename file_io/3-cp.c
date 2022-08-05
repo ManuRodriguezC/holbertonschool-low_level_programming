6ffefda896909c171a8ce0a680a06c3c7a9115c2
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-04 20:58:07 -0500

    copy any file in to other file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 	}
 	if (rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	}
 	clrd = close(fd_read);
