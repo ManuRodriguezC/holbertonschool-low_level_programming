51c9bcd8fad52cfc74fa70b49ebc2d9fc652e0da
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-05 09:36:52 -0500

    Add a new node at the begin list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,16 +10,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	new = *head;
-
 	new = malloc(sizeof(dlistint_t));
-
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	(*head)->prev = new;
 	new->next = *head;
 	*head = new;
-
 	return (*head);
 }
