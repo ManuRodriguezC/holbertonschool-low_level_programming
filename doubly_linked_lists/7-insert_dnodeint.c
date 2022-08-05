8c027da44efba3366173b31d9198fe0fd5412b8a
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-05 18:53:44 -0500

    Add a new node in ramdom position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL || h == NULL)
 		return (NULL);
 	new->n = n;
-	temp = *h
+	temp = *h;
 	if (idx == 0);
 	{
 		*h = new;
