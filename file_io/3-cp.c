aed1c6013ea80151f16b8cbc6da7030030bf7a59
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-04 19:46:38 -0500

    Creat and copy any file in other file

---
+++ file_io/3-cp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ *main - Write a program that copies the content of a file to another file.
+ *@argc: Number of argumnets in the fuction.
+ *@argv: Argumens vector.
+ *Return: 0 or success
+ */
+int main(arvc, *argv[])
+{
+	int fd_read, fd_write, wr, rd;
+	char *buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_read = open(argv[1], O_RDONLY);
+	rd = read(fd_read, buf, 1024); 
+	if (!fd_read)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_write = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	wr = write(fd_write, buf, rd); 
+	if (!fd_write || !wr)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	close(fd_read);
+	close(fd_write);
+	if (fd_read)
+	{
+		dprintf(STDERR_FILENO"Error: Can't close %d\n", fd_read);
+		exit(100);
+	}
+	if (fd_write)
+	{
+		dprintf(STDERR_FILENO"Error: Can't close %d\n", fd_write);
+		exit(100);
+	}
+	return(0);
+}
