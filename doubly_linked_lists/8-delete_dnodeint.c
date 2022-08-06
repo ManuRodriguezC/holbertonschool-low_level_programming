6774c71b92f06c3d6dc4c434ea4e14cd73dc4f82
ManuRodriguezC <estebanclimb@gmail.com>
2022-08-05 19:24:10 -0500

    Deleted node at given index

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		free(temp);
+		if (*head != NULL)
+			free(temp);
 		return (1);
 	}
 	while (temp->next != NULL)
