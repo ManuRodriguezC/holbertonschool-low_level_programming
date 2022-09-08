a7b6669683302c609d2129147ef01b0b8f96eace
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-07 21:01:24 -0500

    Add to the function that search of binary algorithm

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,27 +1,42 @@
 #include "search_algos.h"
 
 /**
- *
- * 
+ * binary_search - Function that searches for a value in a sorted array
+ * of integers using the Binary search algorithm
+ * @array: The array to searches for a value.
+ * @size: The size of the array.
+ * @value: Value to search in the array.
+ * Return: -1 if the value not search in array or the position
+ * of the value in the array.
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t position = 0, len = 2;
-    int temp;
+	int position = 0, count = 0, last = size - 1, med;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    while (len <= size)
-    {
-        printf("Searching in array: ");
-        while (position < size)
-        {
-            temp = array[position];
-            printf("%d, ", temp);
-            position++;
-        }
-        size /= 2;
-        printf("\n");
-    }
+	while (position <= last)
+	{
+		count = 0;
+		printf("Searching in array: ");
+		count = position;
+		while (count <= last)
+		{
+			if (count == last)
+				printf("%d\n", array[count]);
+			if (count != last)
+				printf("%d, ", array[count]);
+			count++;
+		}
+		med = (position + last) / 2;
+
+		if (array[med] < value)
+			position = med + 1;
+		else if (array[med] > value)
+			last = med - 1;
+		else
+			return (med);
+	}
+	return (-1);
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -11,6 +11,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int bs(int *array, int l, int r, int x);
 
 
 #endif
