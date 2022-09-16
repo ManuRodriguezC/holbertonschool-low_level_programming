e0723385563d66eace8757a58acb47b1a3b92253
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-15 20:24:59 -0500

    search interpolation

---
+++ search_algorithms/102-interpolation.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - function that searches for a value in a sorted
+ * array of integers using the Interpolation search
+ * @array: array that search to position of the value
+ * @size: size of the array
+ * @value: value to search in array
+ * Return: position of the value or -1 if value is out of range
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int low = 0, higt = size - 1, pos = 0;
+
+	while (low <= higt && value >= array[low] && value <= array[higt])
+	{
+		pos = low + ((value - array[low]) * (higt - low))
+			/ (array[higt] - array[low]);
+		printf("Value checked array[%d] = [%d]\n", pos, array[pos]);
+		if (value > array[pos])
+			low = pos + 1;
+		else if (value < array[pos])
+			higt = pos - 1;
+		else
+			return (pos);
+	}
+	if (value > array[higt])
+		printf("Value checked array[2109] is out of range\n");
+	return (-1);
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -13,6 +13,8 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int inter_search(int *array, int low, int high, int value);
 
 
 #endif
